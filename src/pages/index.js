9376d9c171f2f370ae01b720ca945c4c9a85ba11
pia smith <basiccreation@yahoo.com>
2019-03-16 23:08:29 -0700

    added background image on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,28 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 
 import SEO from "../components/seo"
-
-const IndexPage = () => (
+import BackgroundSection from "../components/BackgroundSection"
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi Very Beautiful People</h1>
-    <p>Added Navbar waiting for it to populate.</p>
-    <p>Now go build something great.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="cup-a-joe"
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
