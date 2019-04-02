8cdcd67cc52a60454c11d630820a52c10f85474a
pia smith <basiccreation@yahoo.com>
2019-04-01 23:56:59 -0700

    filled rest of information of product card out

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,13 @@ export default function Product({ product }) {
     <div className="col-10 col-sm-8 col-md-6 col-lg-4">
       <div className="card" style={{ minHeight: "100%" }}>
         <Img fluid={product.image.fluid} className="card-img-top" />
+        <div className="card-body text-center">
+          <h6>{product.title}</h6>
+          <h6>${product.price}</h6>
+          <button className="btn btn-yellow mt-3 text-capitalize">
+            Add to Cart
+          </button>
+        </div>
       </div>
     </div>
   )
